8a1a789cf409143f0a79cac0510c3781c2abad96
Nguyen Tran <knguyen.uit@gmail.com>
2020-03-09 01:07:14 +0700

    Init project

---
+++ MapDemo/MapDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDemo
+//
+//  Created by kn7's Macbook on 3/9/20.
+//  Copyright © 2020 Nguyen Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
